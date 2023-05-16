356a4048ad4a7a4f102b4ed28a57eef4761b091f
xlzior <wenjun.lye@gmail.com>
2023-05-16 12:55:50 +0800

    Fix timezone

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,10 +1,6 @@
 function formatDateTime(date: Date): string {
-  const YYYY = date.getFullYear()
-  const MM = String(date.getMonth() + 1).padStart(2, '0')
-  const DD = String(date.getDate()).padStart(2, '0')
-  const hh = String(date.getHours()).padStart(2, '0')
-  const mm = String(date.getMinutes()).padStart(2, '0')
-  return `${YYYY}-${MM}-${DD}T${hh}:${mm}:00`
+  date.setHours(date.getHours() + 8)
+  return date.toISOString().substring(0, 19)
 }
 
 export function getDateTime(): string {
